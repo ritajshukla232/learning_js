0231af9c5defd7bd4a278d7c3e5a256f51013f72
ritajshukla232 <shuklaritaj@gmail.com>
2024-09-05 10:37:45 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+// "use strict" treat all js code as newer version
+console.log("hello")
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object 
+
+/*
+number 
+bigint - large number of data
+string -""
+boolean - true/ false
+null - standlone value
+undefined
+symbol - unique 
+object
+ */
