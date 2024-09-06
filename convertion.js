e5b25282853eed89e51b235b426d780a8504c694
ritajshukla232 <shuklaritaj@gmail.com>
2024-09-06 23:17:47 +0530

    Add files via upload

---
+++ convertion.js
@@ -0,0 +1,14 @@
+let isLoggedIn = null
+let booleanIsLoggedIn =Boolean (isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+/*
+ 1- true; 0 -false;
+ "" - false ; "jfkf" - true
+ undefined - false ; null - false ; 
+  */
+ let someNumber = 33;
+ let stringNumber = String(someNumber);
+ console.log(someNumber);
+ console.log(typeof stringNumber)
+
