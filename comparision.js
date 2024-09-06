ebbbe61da558b73d56966370e965cd1b447e76cb
ritajshukla232 <shuklaritaj@gmail.com>
2024-09-06 23:18:23 +0530

    Add files via upload

---
+++ comparision.js
@@ -0,0 +1,22 @@
+// basic comparision
+/*console.log(2 > 1);
+console.log(4 >= 1);
+console.log(3 < 2);
+console.log(2 == 1);
+console.log(2 !=1 );
+*/
+
+// below comaparision are confused us so avoid that type of comparision
+
+
+console.log(null > 0); //false
+console.log(null >= 0); // only this one true
+console.log(null == 0); //false
+
+console.log(undefined > 0); //false
+console.log(undefined >= 0); //false
+console.log(undefined == 0); //false
+
+
+// strict cheker
+console.log("2" === 2)  //false
