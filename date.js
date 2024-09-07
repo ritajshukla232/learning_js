76885b0a2a8d8ec833e0751b9fa756f96ab96083
ritajshukla232 <shuklaritaj@gmail.com>
2024-09-07 20:23:00 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,14 @@
+// date
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toDateString());
+console.log(myDate.getDate());
+console.log(myDate.getMonth() +1);
+console.log(myDate.getFullYear());
+console.log(myDate.getDay()+1);
+
+
+
