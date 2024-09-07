12b8b8936b8fb3d45a1da7dd5a72bfc4500b61ba
ritajshukla232 <shuklaritaj@gmail.com>
2024-09-07 20:37:56 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,13 @@
+
+let num = Math.random()
+//  console.log(num)
+ const min = 1;
+ const max= 6;
+//  console.log((Math.random()*10) +1);
+const value =(Math.floor(Math.random () * (max - min +2 )))
+console.log(value)
+if (value == 6){
+    const value5 =(Math.floor(Math.random () * (max - min +1)))
+    console.log(value5)
+}
+
