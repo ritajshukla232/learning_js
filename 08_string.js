8ba942256359c332cb4695ef918b7f52123f33e1
ritajshukla232 <shuklaritaj@gmail.com>
2024-09-08 17:55:25 +0530

    Add files via upload

---
+++ 08_string.js
@@ -0,0 +1,15 @@
+const name = "ritaj";
+const repoCount =50;
+console.log(`my name is ${name} and my repoCount is ${repoCount}`)
+
+//check the string functions how this works
+
+const gameName = new String (`ritaj-hy`);
+console.log( gameName.length)
+// console.log( gameName[4])
+console.log(gameName.toUpperCase()) // change to UPPERCASE
+//------------------------------------------------------------------
+const newString ="      shukla.  ";
+console.log(newString);
+console.log(newString.trim()) //ignore all unneccessary spaces
+
