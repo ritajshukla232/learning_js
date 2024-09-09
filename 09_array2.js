871bb1a0681a9cd0856aa54f59fcfe7ab0663b2c
ritajshukla232 <shuklaritaj@gmail.com>
2024-09-09 23:00:55 +0530

    Add files via upload

---
+++ 09_array2.js
@@ -0,0 +1,21 @@
+// use of (for of )loop in js
+let cities = ["akbarpur", "kanpur" ,"lucknow" ,"noida","delhi"]
+for( let city of cities){
+    console.log(city)
+}
+
+//concat method in js
+let pet = ["cat", "dog","cow",];
+let wild = ["lion","tiger","elephant"]
+
+let animal = pet.concat(wild);
+console.log(animal);
+
+//use of(array.isarray) & (array.from())
+
+
+console.log(Array.isArray("ritaj shukla"));
+console.log(Array.from("shukla"));
+
+
+
